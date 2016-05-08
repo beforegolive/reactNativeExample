b6387d92479cb679fd707a80fe3e6bc05ce6be47
JackyHsu <coder_jackyhsu@163.com>
2016-05-08 21:54:11 +0800

    complete android part

--- index.android.js
+++ index.android.js
@@ -3,101 +3,38 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  NavigatorIOS,
-  TextInput,
-  Switch,
-  StatusBar,
-  TouchableHighlight,
-  TouchableOpacity,
-  Picker,
-  Navigator
-} from 'react-native';
+ import React, {
+   AppRegistry,
+   Component,
+   StyleSheet,
+   Text,
+   View,
+   TextInput,
+   TouchableHighlight,
+   Navigator
+ } from 'react-native';
 
-import MyView from './myFirstView';
-import MyNavigator from './myNavigator';
+ import { Provider } from 'react-redux'
+ import { createStore, combineReducers } from 'redux'
+ import { searchBook, ebookDetail, setSettings } from './src/reducer.js'
+ import SearchResultList2 from './src/searchResultList'
+ import SearchBox from './src/searchBox'
+ import BookDetail from './src/bookDetail'
+ import TabNavigator from 'react-native-tab-navigator'
+ import Settings from './src/settings'
+ import App from './src/app'
 
-class AwesomeProject extends Component {
-  // var MOCKED_MOVIES_DATA=[
-  //   {title:'Title', year:'2015', posters: {thumbnail:'
-  //     http://i.imgur.com/UePbdph.jpg'}}
-  // ];
-  constructor(){
-    super();
-    this.state={
-      secondSwitchValue:false,
-      firstSwitchValue:true,
-      language:'js'
-    };
-  }
-  LoadImage(e){
-    console.log('Hello World From Jacky!');
-    console.log(e.nativeEvents);
-    console.log(Image);
-  }
-  textLayout(e){
-    console.log('In TextLayout');
-    console.log(e);
-  }
-  textPress(e){
-    console.log('In textPress');
-    console.log(e);
-    alert(e);
-  }
-  render() {
-    return (
-      <MyNavigator />
-    );
-  }
-}
+ var reducers = combineReducers({searchBook, ebookDetail, setSettings})
+ var store = createStore(reducers)
 
-  // <MyNavigator />
+ class ebookFinder extends Component {
+   render() {
+     return (
+     <Provider store={store}>
+       <App />
+     </Provider>
+     );
+   }
+ }
 
-// <Picker selectedValue={this.state.language} style={{width:100,height:30}}
-//   onValueChange={(lang) => this.setState({language: lang})} >
-//   <Picker.Item label='Java' value='java'/>
-//   <Picker.Item label='JavaScript' value='js'/>
-// </Picker>
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  icon: {
-    width:100,
-    height:100
-  },
-  redborder: {
-    borderWidth: 1,
-    borderColor:'#ff0000',
-  },
-  inputBox:{
-    width:200,
-    height:20,
-    borderColor:'gray',
-    borderWidth:1,
-    // marginHorizontal: 100
-    alignSelf:'center'
-  }
-});
-
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => ebookFinder);
